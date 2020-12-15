9c18059a4c7c82be0021cbec84fe445bd7ef396d
acse-xw1620 <xiangcen.wu20@imperial.ac.uk>
2020-12-15 23:00:27 +0000

    Add a new python file

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 nothing
 ## heading 2
 # ADD SOMETHING
+fdsfsdafds
---
+++ simple.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+a = np.arange(0, 10, 0.1)
+y = np.exp(-a)
+
+fig = plt.figure(figsize = (12, 4))
+ax = fig.add_subplot(111)
+ax.plot(a, y, 'r', label = 'simple exp function')
+ax.grid()
+ax.legend(loc = 'best', fontsize = 15)
+ax.set_title('My title')
+plt.show()
+
