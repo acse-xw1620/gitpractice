e4d6620f174704a688ee4b39ea0f067a5f3cb53c
acse-xw1620 <xiangcen.wu20@imperial.ac.uk>
2021-01-03 02:12:41 +0000

    very good

--- combine.py
+++ combine.py
@@ -128,21 +128,17 @@ ax.plot(x_nodes, u_ic, 'r', label = 'exact_element')
 
 
 #################################
-import numpy as np
-import matplotlib.pyplot as plt
+
 
 # physics parameters
-kappa = 0.01
-U = 0.2
-# mesh information
-Lx = 1
-N_nodes = 101
-N_elements = N_nodes - 1
-x_nodes = np.linspace(0, Lx, N_nodes)
-dx = np.diff(x_nodes)[0]
-
-def initial_condition(x): return np.exp(-(x - 0.5)**2 / 0.005)
-u_ic = initial_condition(x_nodes)
+# kappa = 0.01
+# U = 0.2
+# # mesh information
+# Lx = 1
+# N_nodes = 101
+# N_elements = N_nodes - 1
+# x_nodes = np.linspace(0, Lx, N_nodes)
+dx = dx[0]
 
 def assemble_adv_diff_disc_matrix_central(U, kappa, L, N):
 
@@ -170,28 +166,10 @@ def assemble_adv_diff_disc_matrix_central(U, kappa, L, N):
     A = np.linalg.inv(M) @ B
     return A, x
 
-# physical parameters
-
-
-# set the RHS boundary value to zero as well as the left
-CE = 0
-
-
-# define number of points in spatial mesh (N+2 including ghose nodes)
-N = 100
-
 # use the function we just wrote to form the spatial mesh and the discretisation matrix 
 A, x = assemble_adv_diff_disc_matrix_central(U, kappa, Lx, N_nodes)
-
 # define a time step size
 dt = 0.001
-
-# and compute and print some key non-dimensional parameters
-dx = Lx / N
-print('Pe_c: {0:.5f}'.format(U*dx/kappa))
-print('CFL: {0:.5f}'.format(U*dt/dx))
-print('r: {0:.5f}'.format(kappa*dt/(dx**2)))
-
 # define the end time and hence some storage for all solution levels
 tend = dt*1000
 # assume a constant dt and so can define all the t's in advance
@@ -199,18 +177,17 @@ t = np.arange(0, tend, dt)
 # and we can also set up a matrix to store the discrete solution in space-time
 # with our a priori knowledge of the size required.
 C = np.empty((len(x),len(t)))
-
 # define an initial condition - a "blob" in the shape of a Gaussian
 # and place it in the first column of the C matrix which stores all solution levels
 C[:, 0] = initial_condition(x)
-
 # now let's do the time-stepping via a for loop
 # we will need the identity matrix so define it once outside the loop
 I = np.eye(len(x))
 for n in range(len(t)-1):
     C[:,n+1] = (I + A * dt) @ C[:, n]
-    
+
 ax.plot(x, C[:,0], label = 'exact_diff')
 ax.plot(x, C[:, -1], label = 'diff')
 ax.legend()
+ax.set_xlim(0, 1)
 plt.show()
